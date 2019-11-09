10429f7761242a72fdf52fc02b3c4260e3845642
Masahiro Masuda <masahi129@gmail.com>
2019-11-09 09:49:10 +0900

    use btree set but slow, need to debug

--- klotski/src/main.rs
+++ klotski/src/main.rs
@@ -1,3 +1,6 @@
+// ported from https://github.com/masahi/ocaml_practice/blob/master/mooc/klotski.ml
+use std::collections::BTreeSet;
+
 fn find_index<T>(pred: impl FnMut(&T) -> bool, vec: &Vec<T>) -> Option<usize> {
     vec.iter().position(pred)
 }
@@ -9,7 +12,7 @@ where
     move |conf_list| conf_list.iter().flat_map(rel).collect()
 }
 
-fn solve<T, F, P>(r: F, p: P, a: T) -> T
+fn solve_slow<T, F, P>(r: F, p: P, a: T) -> T
 where
     F: Fn(&T) -> Vec<T> + Copy,
     P: FnMut(&T) -> bool + Copy,
@@ -27,11 +30,55 @@ where
     iter(vec![a], r, p)
 }
 
+
+fn archive_map<T, F>(r: F, (s,l) : (BTreeSet<T>, Vec<T>)) -> (BTreeSet<T>, Vec<T>)
+where
+    F: Fn(&T) -> Vec<T> + Copy,
+    T: Clone + Ord
+{
+    l.into_iter().fold((s, vec![]), |(seen, new_elts), x| {
+        r(&x).into_iter().fold((seen, new_elts), |(mut set, mut frontiers), elt| {
+            if set.contains(&elt) {
+                (set, frontiers)
+            } else {
+                frontiers.push(elt.clone());
+                set.insert(elt);
+                (set, frontiers)
+            }
+        })
+    })
+}
+
+fn solve<T, F, P>(r: F, p: P, a: T) -> T
+where
+    F: Fn(&T) -> Vec<T> + Copy,
+    P: FnMut(&T) -> bool + Copy,
+    T: Clone + Ord
+{
+    fn iter<T, F, P>(r: F, p: P, s: BTreeSet<T>, mut l: Vec<T>) -> T
+    where
+        F: Fn(&T) -> Vec<T> + Copy,
+        P: FnMut(&T) -> bool + Copy,
+        T: Clone + Ord
+    {
+        match find_index(p, &l) {
+            Some(ind) => l.remove(ind),
+            None => {
+                let (s, l) = archive_map(r, (s, l));
+                iter(r, p, s, l)
+            }
+        }
+    }
+    let mut init_set = BTreeSet::new();
+    init_set.insert(a.clone());
+    iter(r, p, init_set, vec![a])
+}
+
 fn solve_path<T, F, P>(r: F, mut p: P, a: T) -> Vec<T>
 where
     F: Fn(&T) -> Vec<T> + Copy,
     P: FnMut(&T) -> bool + Copy,
-    T: Clone
+    T: Clone + Ord
 {
     let path_rel = move |path: &Vec<T>| {
         match path.last() {
@@ -55,6 +102,7 @@ where
     };
     solve(path_rel, path_prop, vec![a])
 }
+
 fn main() {
     let near = |n: &i32| vec![*n - 2, *n - 1, *n, *n + 1, *n + 2];
     let new_rel = flat_map(near);
